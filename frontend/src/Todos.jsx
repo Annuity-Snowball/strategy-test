517ae6437c119e1a0eae7dcf418b4834d553cbbb
cananybodyhearme <ssk56545442@gmail.com>
2022-07-25 06:58:27 +0900

    frontend http changed

--- frontend/src/Todos.jsx
+++ frontend/src/Todos.jsx
@@ -28,7 +28,7 @@ const TodosContext = React.createContext({
 export default function Todos() {
   const [strategy_list, setTodos] = useState([])
   const fetchTodos = async () => {
-    const response = await fetch("http://192.168.30.4:8000/stra")
+    const response = await fetch("http://localhost:8000/stra")
     const strategy_list = await response.json()
     setTodos(strategy_list.data)
   }
@@ -75,7 +75,7 @@ function AddTodo() {
       "age"   : age
     }
 
-    fetch("http://192.168.30.4:8000/stra", {
+    fetch("http://localhost:8000/stra", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newstrategy_list)
