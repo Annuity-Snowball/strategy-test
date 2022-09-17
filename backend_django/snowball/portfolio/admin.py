f6c99c008b25a492b74bd4b71b4eedf56b4d5eae
cananybodyhearme <ssk56545442@gmail.com>
2022-09-18 01:09:33 +0900

    this is wht

--- backend_django/snowball/portfolio/admin.py
+++ backend_django/snowball/portfolio/admin.py
@@ -7,6 +7,5 @@ from .models import *
 admin.site.register(User)
 admin.site.register(Portfolio_info)
 admin.site.register(Product_info)
-admin.site.register(Portfolio_result)
-admin.site.register(Notice)
+admin.site.register(Portfolio_result_temp)
 
